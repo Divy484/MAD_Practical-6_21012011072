66b202866d5baae6fc00f1eaa573882ba92698f2
Divy484 <98522523+Divy484@users.noreply.github.com>
2023-09-11 08:23:03 +0530

    Practical-6

--- app/src/main/java/com/example/mad_practical_6_21012011072/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_6_21012011072/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.mad_practical_6_21012011072
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val start : Button = findViewById(R.id.play_button)
+        start.setOnClickListener { play() }
+
+        val stop : Button = findViewById(R.id.stop_button)
+        stop.setOnClickListener { pause() }
+    }
+
+    fun play(){
+
+    }
+
+    fun pause(){
+
     }
 }
